5db333bbe5db93f2228d6821d514f2bbaea47651
Konstantinos Kousias <kostas.kousias@gmail.com>
2021-07-01 14:41:12 +0200

    Update hindtest.R

--- HINDSIGHT/hindtest.R
+++ HINDSIGHT/hindtest.R
@@ -6,7 +6,7 @@ source('hindsightpp.R') # Load main function
 files = c('../pollution.csv') # Input files (separated by comma)
 
 res = hindsightpp(files, 
-                  nfeatures = 6, nlags = 5, msteps = 2, fs_method = 'rfe',
-                  network_type = 'Vanilla', act = 2, opt = 2, 
-                  split = 0.8, valsplit = 0.2, search_type = "Manual", niter = 2, 
+                  nfeatures = 6, nlags = 5, msteps = 1, fs_method = 'rfe',
+                  network_type = 'Vanilla', act = 3, opt = 2, 
+                  split = 0.8, valsplit = 0.2, search_type = "Manual", niter = 50, 
                   units1 = 256, units2 = 128, units3 = 64, lr = 0.001, nepochs = 50, bs = 8, nlayers = 2)
